dc0e28e5e223e58f0582b8682615539b65e96386
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-09-15 11:13:28 -0400

    added

---
+++ Game Studio 1/GameProjects/Inner Thing/Assets/Scripts/CandleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CandleController : MonoBehaviour
+{
+    public GameObject CandleUnlit;
+    public GameObject Candle_Animation;
+
+    private void OnMouseUp()
+    {
+        // Lit the candle
+        CandleUnlit.SetActive(false);
+        Candle_Animation.SetActive(true);
+        // Broadcast a message
+        Fungus.Flowchart.BroadcastFungusMessage("Candle is lit");
+
+    }
+}
