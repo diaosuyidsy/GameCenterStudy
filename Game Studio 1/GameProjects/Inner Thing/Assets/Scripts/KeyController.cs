8996750ba2044098e8a38be3209e7db8ec1303bc
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-09-23 13:06:22 -0400

    added

--- Game Studio 1/GameProjects/Inner Thing/Assets/Scripts/KeyController.cs
+++ Game Studio 1/GameProjects/Inner Thing/Assets/Scripts/KeyController.cs
@@ -1,14 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Fungus;
+﻿using UnityEngine;
+using System.Collections;
 
 public class KeyController : MonoBehaviour
 {
-    private void OnMouseEnter ()
+    public GameObject[] Locations;
+
+    private bool ClickedKey;
+    private Vector3[] Locs;
+    private int LocIndex = 0;
+
+    private void Start ()
     {
-        Debug.Log ("hello");
+        Locs = new Vector3[Locations.Length];
+        for (int i = 0; i < Locations.Length; i++)
+        {
+            Locs[i] = Locations[i].transform.position;
+        }
+    }
+
+    public void Appear ()
+    {
+        GetComponent<SpriteRenderer> ().enabled = true;
+        GetComponent<BoxCollider2D> ().enabled = true;
+        StartCoroutine (Teleport ());
+    }
+
+    private void OnMouseUp ()
+    {
+        ClickedKey = true;
         Fungus.Flowchart.BroadcastFungusMessage ("Found the key");
 
+    }
+
+    IEnumerator Teleport ()
+    {
+        while (!ClickedKey)
+        {
+            transform.position = Locs[LocIndex++];
+            if (LocIndex >= Locs.Length)
+            {
+                LocIndex = 0;
+            }
+            yield return new WaitForSeconds (15f);
+        }
     }
 }
